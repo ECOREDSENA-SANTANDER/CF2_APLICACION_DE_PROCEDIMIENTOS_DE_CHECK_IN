f71cf05288f7a3c2ee49bd027ac4c7f02066a839
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-08-05 20:26:27 -0500

    avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -26,8 +26,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/uUcVZQl-LE4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Guía práctica en el procedimiento de #[em check-in] - Introducción
 
 </template>
 
